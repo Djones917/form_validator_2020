7b2c60b574eb2e795e73628006b520b354659cf1
Derek <nightwing917@hotmail.com>
2020-03-16 20:21:26 -0600

    done with form validation 2020

--- refactored.js
+++ refactored.js
@@ -28,9 +28,13 @@ function showSuccess(input) {
 
 
 // Check if email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())) {
+      showSuccess(input);
+    } else {
+      showError(input, 'Email is not valid!');
+    }
 }
 
 
@@ -67,6 +71,16 @@ function checkLength(input, min, max) {
 
 
 
+ // Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
+
+
+
 // Get field name
 function getFieldName(input) {
    // Make first letter uppercase then add the rest
@@ -83,4 +97,6 @@ form.addEventListener('submit', function(e) {
    checkRequired([username, email, password, password2]);
    checkLength(username, 3, 15);
    checkLength(password, 6, 25);
+   checkEmail(email);
+   checkPasswordsMatch(password, password2);
 });
